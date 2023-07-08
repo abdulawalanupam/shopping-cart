2a50f904b2b74e2e7c2e7467ba55f36663f51f5e
Abdul Awal Anupam <78092761+abdulawalanupam@users.noreply.github.com>
2023-07-08 19:51:26 +0600

    1st function ok

--- script.js
+++ script.js
@@ -1,29 +1,12 @@
-/* document.getElementById("plusBtn").addEventListener("click", function () {
-  const itemQty = document.getElementById("itemQty").value;
-  const itemQtyNumber = parseInt(itemQty);
-  const totalQty = itemQtyNumber + 1;
-  document.getElementById("itemQty").value = totalQty;
-}); */
-
-
-document.getElementById("plusBtn").addEventListener("click",itemPlusMinus('itemQty'));
-
-document.getElementById("plusBtn").addEventListener("click", ){
-
-    function itemPlusMinus(id){
-        const itemQty = document.getElementById(id).value;
-        const itemQtyNumber = parseInt(itemQty);
-        const totalQty = itemQtyNumber + 10;
-        document.getElementById(id).value = totalQty;
-    }
+function itemPlusMinus(trueFalse) {
+  const itemQty = document.getElementById("itemQty");
+  const itemQtyNumber = parseInt(itemQty.value);
+  if (trueFalse == true) {
+    totalQty = itemQtyNumber + 1;
+  } else if (trueFalse == false && itemQtyNumber > 0) {
+    totalQty = itemQtyNumber - 1;
+  }
+  const newQty = (document.getElementById("itemQty").value = totalQty);
+  const itemPrice = document.getElementById("itemPrice");
+  document.getElementById("itemPrice").innerText = 1219 * newQty;
 }
-
-
-
-
-document.getElementById("minusBtn").addEventListener("click", function () {
-  const itemQty = document.getElementById("itemQty").value;
-  const itemQtyNumber = parseInt(itemQty);
-  const totalQty = itemQtyNumber - 1;
-  document.getElementById("itemQty").value = totalQty;
-});
